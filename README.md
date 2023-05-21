9f2bfa684bed936ea3bc7e6b011e89756ee0f213
Karece <ecerakle@gmail.com>
2023-05-21 22:54:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# A Paystack payment gateway implementation
+
+This project demonstrates the integration of the Paystack payment gateway into a Laravel application. Paystack is a popular payment gateway in Nigeria that enables businesses to accept online payments securely.
+
+## Features
+
+-   Initiate payment transactions using Paystack's API.
+-   Handle payment callbacks and verify transaction status.
+-   Display payment form and process user inputs.
+-   Provide seamless payment experience to users.
+
+## Resources
+
+-   Laravel documentation: https://laravel.com/docs
+-   Paystack documentation: https://paystack.com/docs
---
+++ resources/views/paystack-payment.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paystack Payment Gateway</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            padding: 20px;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input[type="email"],
+        input[type="tel"],
+        input[type="text"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        .form-submit {
+            text-align: center;
+        }
+
+        button[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+
+<body>
+    <form id="paymentForm">
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="email" id="email-address" required />
+        </div>
+        <div class="form-group">
+            <label for="amount">Amount</label>
+            <input type="tel" id="amount" required />
+        </div>
+        <div class="form-group">
+            <label for="first-name">First Name</label>
+            <input type="text" id="first-name" />
+        </div>
+        <div class="form-group">
+            <label for="last-name">Last Name</label>
+            <input type="text" id="last-name" />
+        </div>
+        <div class="form-submit">
+            <button type="submit" onclick="payWithPaystack(event)"> Pay </button>
+        </div>
+    </form>
+
+    <script src="https://js.paystack.co/v1/inline.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script>
+        const paymentForm = document.getElementById("paymentForm");
+        paymentForm.addEventListener("submit", payWithPaystack, false);
+
+        function payWithPaystack(e) {
+            e.preventDefault();
+
+            let handler = PaystackPop.setup({
+                key: 'pk_test_94eede8f12d1808de281af19b27600fd6f483718', // Replace with your public key
+                email: document.getElementById("email-address").value,
+                amount: document.getElementById("amount").value * 100,
+                ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+                // label: "Optional string that replaces customer email"
+                onClose: function() {
+                    alert('Window closed.');
+                },
+                callback: function(response) {
+                    let reference = response.reference;
+                    //verify payment
+                    console.log(reference);
+                    // let message = 'Payment complete! Reference: ' + response.reference;
+                    // alert(message);
+                }
+            });
+
+            handler.openIframe();
+        }
+    </script>
+</body>
+
+</html>
